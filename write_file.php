40a808291ef5c4621ac8902239e4509620a7483a
Rommel Sunga <rommel.sunga@gmail.com>
2018-02-27 09:37:10 -0800

    Local PHP file path

--- write_file.php
+++ write_file.php
@@ -1,11 +1,11 @@
 <?php
 
     $data = "test data \n";
-    $fp = '/Users/rommelsunga/Projects/php_vs_go_write_file/write_file_php.txt';
+    $fp = './write_file_php.txt';
 
     $time_start = microtime(true);
     for($i = 0; $i < 100000; $i++) {
-        file_put_contents($fp, $data, FILE_APPEND);
+        file_put_contents($fp, $data, FILE_APPEND | FILE_EX | FILE_NB);
     }
     $time_end = microtime(true);
     $time = $time_end - $time_start;
